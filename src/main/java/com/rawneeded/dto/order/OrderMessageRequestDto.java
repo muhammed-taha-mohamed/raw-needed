1d5a947b7d26e9993217343fa0181b8fee6a56c1
mohamedtahaomk35@gmail.com <muhammed11taha@@>
2026-01-26 09:30:33 +0000

    CHAT

--- src/main/java/com/rawneeded/dto/order/OrderMessageRequestDto.java
+++ src/main/java/com/rawneeded/dto/order/OrderMessageRequestDto.java
@@ -17,29 +17,3 @@ public class OrderMessageRequestDto {
     private String image;
 }
 
-
-
-ضيف اوبشن شات مع الطلبات بين المورد والعمليل مع كل طلب
-يظهر وقت ما تظهر ايقونه التفاوض يعني لما المورد يرد او حاله
-ال apis
-get messages by order :
-GET : https://45.94.58.36:8787/raw-needed/api/v1/orders/order-id/messages
-response :
-list of :
-public class OrderMessageResponseDto {
-private String id;
-private String orderId;
-private String userId;
-private String userName;
-private String userOrganizationName;
-private String message;
-private String image;
-private LocalDateTime createdAt;
-}
-ADD MESSAGE
-POST
-https://45.94.58.36:8787/raw-needed/api/v1/orders/orderid/messages
-BODY : {
-"message": "string",
-"image": "string"
-}
